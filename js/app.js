82a49ce7975351f17b2d1b7b27afcc948c980cb2
Cory Reed <swashcap@gmail.com>
2015-04-23 10:35:43 -0700

    Build out application bootstrapping file.

---
+++ js/app.js
@@ -0,0 +1,17 @@
+window.React = require('React');
+
+var ProductData = require('./ProductData');
+var CartAPI = require('./utils/CartAPI');
+var FluxCartApp = require('./components/FluxCartApp.react');
+
+// Load mock product data into `localStorage`
+ProductData.init();
+
+// Load mock API call
+CartAPI.getProductData();
+
+// Render `FluxCartApp` controller view
+React.render(
+    <FluxCartApp />
+    document.getElementById('flux-cart')
+);
