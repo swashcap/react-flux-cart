ab69120863f8966ff4ab25579681f9a76b80d133
Cory Reed <swashcap@gmail.com>
2015-04-25 11:34:58 -0700

    Fix app's React package reference.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var React = require('React');
+var React = require('react');
 var ProductData = require('./ProductData');
 var CartAPI = require('./utils/CartAPI');
 var FluxCartApp = require('./components/FluxCartApp.react');
